8d37298c953b26fab586938a480779e7caa6f8c8
aiolos404 <aiolos404@gmail.com>
2015-03-01 10:35:05 -0600

    init upload

---
+++ config/config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+
+module.exports = {
+    "pastebin" : {
+        "api_dev_key" : "5cae169fb42133ac971afa40e0bffd0e",
+        "api_user_key" : null,
+        "api_user_name" : "aioloscool",
+        "api_user_password" : "86563178",
+    },
+};
+
+
